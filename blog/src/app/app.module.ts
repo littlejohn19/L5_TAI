17c435bb298c5c1046adf3910a2eda5d6b663f2d
krystian <krystian.cwioro@o2.pl>
2021-03-22 23:32:14 +0100

    navbar

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogComponent,
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
