c23c438a3fea51691f329c749d6e7d1282af6b36
Ejdzyn <adrian.wstepnik@gmail.com>
2021-03-22 14:53:48 +0100

    Transpilacja

--- complex.js
+++ complex.js
@@ -1,25 +1,25 @@
-var Complex = /** @class */ (function () {
-    function Complex(real, imaginary) {
+"use strict";
+class Complex {
+    constructor(real, imaginary) {
         this.real = real;
         this.imaginary = imaginary;
     }
-    Complex.prototype.add = function (object) {
+    add(object) {
         return new Complex(this.real + object.real, this.imaginary + object.imaginary);
-    };
-    Complex.prototype.sub = function (object) {
+    }
+    sub(object) {
         return new Complex(this.real - object.real, this.imaginary - object.imaginary);
-    };
-    Complex.prototype.module = function () {
+    }
+    module() {
         return Math.sqrt(Math.pow(this.real, 2) + Math.pow(this.imaginary, 2));
-    };
-    Complex.prototype.toString = function () {
+    }
+    toString() {
         console.log("RZECZYWISTA: " + this.real + (this.real != 0 ? ", UROJONA: " + this.imaginary : ""));
-    };
-    return Complex;
-}());
-var c1 = new Complex(3, 4);
-var c2 = new Complex(6, 2);
-var result = c1.add(c2);
+    }
+}
+const c1 = new Complex(3, 4);
+const c2 = new Complex(6, 2);
+let result = c1.add(c2);
 result.toString();
 result = result.sub(c1);
 result.toString();
