f1ebf3806e30b7336c44ae1521fda787865f2235
Kinga <kinga2233@onet.pl>
2021-03-26 17:37:22 +0100

    #Ex 2

--- complex.js
+++ complex.js
@@ -1,31 +1,31 @@
-var Complex = /** @class */ (function () {
-    function Complex(real, imaginary) {
+"use strict";
+class Complex {
+    constructor(real, imaginary) {
         this.real = real;
         this.imaginary = imaginary;
     }
-    Complex.prototype.addOperations = function (firstNumber) {
+    addOperations(firstNumber) {
         return new Complex(this.real + firstNumber.real, this.imaginary + firstNumber.imaginary);
-    };
-    Complex.prototype.subOperations = function (firstNumber) {
+    }
+    subOperations(firstNumber) {
         return new Complex(this.real - firstNumber.real, this.imaginary - firstNumber.imaginary);
-    };
-    Complex.prototype.moduloOperations = function () {
+    }
+    moduloOperations() {
         return Math.sqrt(Math.pow(this.real, 2) + Math.pow(this.imaginary, 2));
-    };
-    Complex.prototype.toString = function () {
+    }
+    toString() {
         console.log('Re:', this.real, ' ', 'Im:', this.imaginary);
-    };
-    return Complex;
-}());
-var b = new Complex(3, 3);
-var a = new Complex(2, 3);
+    }
+}
+let b = new Complex(3, 3);
+let a = new Complex(2, 3);
 //addOperations(a).toString();
-var sum = a.addOperations(b);
+let sum = a.addOperations(b);
 console.log('Sum of object: ');
 sum.toString();
-var substraction = a.subOperations(b);
+let substraction = a.subOperations(b);
 console.log('Substraction of object: ');
 substraction.toString();
-var modulo = sum.moduloOperations();
+let modulo = sum.moduloOperations();
 console.log('Modulo of sum');
 console.log(modulo);
