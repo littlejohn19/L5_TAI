938c6f9d6f0dace8100322a475b61eb81cc96d89
Przemysław Kuca <evildemage@gmail.com>
2021-04-23 16:07:27 +0200

    Posts and style

--- blog/src/app/components/blog-item-details/blog-item-details.component.ts
+++ blog/src/app/components/blog-item-details/blog-item-details.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {DataService} from "../../services/data.service";
 
 @Component({
   selector: 'blog-item-details',
@@ -6,12 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog-item-details.component.css']
 })
 export class BlogItemDetailsComponent implements OnInit {
-  image = 'http://osnews.pl/wp-content/uploads/2016/06/it-grafika.jpg';
-  text = 'Tytuł';
+  public image = '';
+  public text: string;
+  public id: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private dataService: DataService, private route: ActivatedRoute) {
   }
+
+  ngOnInit() {
+    let id: string | null = '';
+    this.route.paramMap
+      .subscribe(params => {
+        id = params.get('id');
+      });
+    this.dataService.getById(id).subscribe(res => {
+      // @ts-ignore
+      this.image = res.image;
+      // @ts-ignore
+      this.text = res.text;
+    });
+
+  }
+
+
+
 
 }
