cf3fdd6964bb44e821b8674e3007a35c1a801922
Boryszs <12damie@gmail.com>
2021-04-23 17:53:09 +0200

    Laboratorium 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { SearchBarComponent } from './shared/search-bar/search-bar.component';
 import { BlogHomeComponent } from './components/blog-home/blog-home.component';
 import { FilterPipe } from './pipes/filter.pipe';
 import { TextFormatDirective } from './directives/text-format.directive';
-
+import { SelectizeComponent } from './components/selectize/selectize.component';
 
 @NgModule({
   declarations: [
@@ -37,6 +37,7 @@ import { TextFormatDirective } from './directives/text-format.directive';
     BlogHomeComponent,
     FilterPipe,
     TextFormatDirective,
+    SelectizeComponent,
   ],
   imports: [
     BrowserModule,
